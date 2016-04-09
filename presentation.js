c8827428119a7dee8767da39764ffe039737c972
Ben Deane <ben@elbeno.com>
2016-04-09 00:40:11 -0700

    WIP, now fairly complete

---
+++ presentation.js
@@ -0,0 +1,16 @@
+(function (window, undefined) {
+    'use strict';
+
+    window.addEventListener('load', function () {
+        window.Reveal.addEventListener('slidechanged', function (event) {
+            var achv = document.getElementById("achv"),
+                newachv = {};
+            // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+            if (event.indexh === 28) {
+                newachv = achv.cloneNode(true);
+                achv.parentNode.replaceChild(newachv, achv);
+            }
+        });
+    });
+
+})(window);
