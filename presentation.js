5f211ec52496121ece9e2e81313cfdd33691e538
Ben Deane <ben@elbeno.com>
2016-04-10 22:51:29 -0700

    Add quotes slide; fix js slide #

--- presentation.js
+++ presentation.js
@@ -6,7 +6,7 @@
             var achv = document.getElementById("achv"),
                 newachv = {};
             // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-            if (event.indexh === 28) {
+            if (event.indexh === 29) {
                 newachv = achv.cloneNode(true);
                 achv.parentNode.replaceChild(newachv, achv);
             }
